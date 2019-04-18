74aced37eff6bbf04135db8838e54bb65f60ac3a
Abbey Dent <adent@g.clemson.edu>
2019-04-18 15:37:16 -0400

    getting currency rows

--- coinmarket_request.py
+++ coinmarket_request.py
@@ -9,7 +9,15 @@ if not os.path.exists("html_files"):
 
 url = 'https://coinmarketcap.com/all/views/all/'
 
-
+for i in range(1):
+	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+	f = open("html_files/coinmarketcap" + current_time_stamp +".html", "wb")
+	response = urllib.request.urlopen('https://coinmarketcap.com/all/views/all/')
+	html = response.read()
+	#print(html)
+	f.write(html)
+	f.close()
+	time.sleep(10)
 
 
 
