6368646f6142a2f1159fbda9d5274139f38e1b2f
Abbey Dent <adent@g.clemson.edu>
2019-04-23 12:19:16 -0400

    creating first dataset

--- coinmarket_parser.py
+++ coinmarket_parser.py
@@ -6,6 +6,8 @@ import pandas as pd
 if not os.path.exists("parsed_files"):
 	os.mkdir("parsed_files")
 
+df = pd.dataframe[]
+
 for one_file_name in glob.glob("html_files/*.html"):
 	print("parsing " + one_file_name)
 	scrapping_time = os.path.splitext(os.path.basename(one_file_name))[0].replace("coinmarketcap","")
@@ -23,28 +25,26 @@ for one_file_name in glob.glob("html_files/*.html"):
 		currency_volume = r.find("a",{"class": "volume"}).text
 		currency_supply = r.find("td", {"class": "circulating-supply"})['data-sort']
 		currency_change = r.find("td", {"class": "percent-change"})['data-sort']
-		print(currency_short_name)
+		#print(currency_short_name)
+		#print(currency_name)
+		#print(currency_market_cap)
+		# print(currency_price)
+		# print(currency_volume)
+		# print(currency_supply)
+		# print(currency_change)
+		df = df.append({
+			'scrapping_time': scrapping_time,
+			'short_name': currency_short_name,
+			'name': currency_name,
+			'market_cap': currency_market_cap,
+			'price': currency_price,
+			'volume': currency_volume,
+			'supply': currency_supply,
+			'24H_percent-change': currency_change
+			}, ignore_index=True)
 
 
-# url = 'https://coinmarketcap.com/all/views/all/'
+df.to_csv("parsed_files/coinmarketcap_dataset1")
+	
 
-# for root, dirs, files in os.walk("html_files"):
-# 	for file in files:
-# 		if file.endswith('.html'):
-# 			print("parsing" + file)
-# 			scrapping_time = os.path.splitext(os.path.basename(file))[0].replace("coinmarketcap", "")
-# 			f = open(file, "r")
-# 			soup = BeautifulSoup(f.read(), 'html.parser')
-# 			f.close()
-# 			currencies_table = soup.find("table", {"id": "currencies-all"})
-# 			currencies_tbody = currencies_table.find("tbody")
-# 			currency_rows = currencies_tbody.find_all("tr")
-# 			for r in currency_rows: 
-# 				currency_short_name = r.find("td", {"class": "currency-name"}).find("span", {"class": "currency-symbol"}).find("a").text
-# 				currency_name = r.find("td", {"class": "currency-name"}).find("a", {"class": "currency-name-container"}).text
-# 				currency_market_cap = r.find("td", {"class": "market-cap"})['data-sort']
-# 				currency_prince = r.find("a", {"class": "price"}).text
-# 				currency_supply = r.find("td", {"class": "circulating-supply"})['data-sort']
-# 				currency_volume = r.find("a", {"class": "volume"}).text
-# 				currency_change = r.find("td", {"class": "percent-change"}, {"data-timespan": "24h"})['data-sort']
 
--- coinmarket_request.py
+++ coinmarket_request.py
@@ -11,48 +11,47 @@ if not os.path.exists("html_files"):
 
 url = 'https://coinmarketcap.com/all/views/all/'
 
-# for i in range(1):
-# 	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-# 	f = open("html_files/coinmarketcap" + current_time_stamp +".html", "wb")
-# 	response = urllib.request.urlopen('https://coinmarketcap.com/all/views/all/')
-# 	html = response.read()
-# 	#print(html)
-# 	f.write(html)
-# 	f.close()
-# 	time.sleep(10)
+for i in range(30):
+	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
+	print("requesting " + url + current_time_stamp)
+	f = open("html_files/coinmarketcap" + current_time_stamp +".html", "wb")
+	response = urllib.request.urlopen('https://coinmarketcap.com/all/views/all/')
+	html = response.read()
+	#print(html)
+	f.write(html)
+	f.close()
+	time.sleep(21600)
 
-f = open("html_files/coinmarketcap.html", "r")
-soup = BeautifulSoup(f.read(), features='lxml')
-f.close()
-currencies_table = soup.find("table", {"id": "currencies-all"})
-currencies_tbody = currencies_table.find("tbody")
-currency_rows = currencies_tbody.find_all("tr")
-crypto_list = []
-crypto_name_list = []
-for r in currency_rows:
-	currency_short_name = r.find("td", {"class": "currency-name"}).find("span", {"class": "currency-symbol"}).find("a").text
-	currency_name = r.find("td", {"class": "currency-name"}).find("a",{"class":"currency-name-container"}).text
-	currency_link = r.find("td", {"class": "currency-name"}).find("a", {"class": "currency-name-container"})['href']
-	crypto_list.append(currency_link)
-	crypto_name_list.append(currency_short_name)
-	#print(crypto_list)
-
-if not os.path.exists("historical_html_files"):
-	os.mkdir("historical_html_files")
+# f = open("html_files/coinmarketcap.html", "r")
+# soup = BeautifulSoup(f.read(), features='lxml')
+# f.close()
+# currencies_table = soup.find("table", {"id": "currencies-all"})
+# currencies_tbody = currencies_table.find("tbody")
+# currency_rows = currencies_tbody.find_all("tr")
+# crypto_list = []
+# crypto_name_list = []
+# for r in currency_rows:
+# 	currency_short_name = r.find("td", {"class": "currency-name"}).find("span", {"class": "currency-symbol"}).find("a").text
+# 	currency_name = r.find("td", {"class": "currency-name"}).find("a",{"class":"currency-name-container"}).text
+# 	currency_link = r.find("td", {"class": "currency-name"}).find("a", {"class": "currency-name-container"})['href']
+# 	crypto_list.append(currency_link)
+# 	crypto_name_list.append(currency_short_name)
+# 	#print(crypto_list)
+# if not os.path.exists("historical_html_files"):
+# 	os.mkdir("historical_html_files")
 
 # print(crypto_list[1:5])
 # print(crypto_name_list[1:5])
 
-for i in range(5):
-	print("requesting " + crypto_name_list[i])
-	# print("historical_html_files/" + crypto_name_list[i]  + ".html")
-	f = open("historical_html_files/" + crypto_name_list[i]  + ".html", "wb")
-	# print("https://coinmarketcap.com" + crypto_list[i]  + "historical-data" + "?start=20130428&end=20190422")
-	response = urllib.request.urlopen("https://coinmarketcap.com" + crypto_list[i]  + "historical-data" + "?start=20130428&end=20190422")
-	html = response.read()
-	f.write(html)
-	f.close()
-	time.sleep(30)
+# for i in range(500):
+# 	print("requesting " + crypto_name_list[i])
+# 	f = open("historical_html_files/" + crypto_name_list[i]  + ".html", "wb")
+# 	# print("https://coinmarketcap.com" + crypto_list[i]  + "historical-data" + "?start=20130428&end=20190422")
+# 	response = urllib.request.urlopen("https://coinmarketcap.com" + crypto_list2[i]  + "historical-data" + "?start=20130428&end=20190422")
+# 	html = response.read()
+# 	f.write(html)
+# 	f.close()
+# 	time.sleep(30)
 
 	 
 			
