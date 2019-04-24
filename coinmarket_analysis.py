363a9ac18d88ea76aa5aec8003a6cb8540dcef57
Abbey Dent <adent@g.clemson.edu>
2019-04-23 21:15:13 -0400

    cleaning a few details

---
+++ coinmarket_analysis.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os
+import pandas as pd
+import time
+from datetime import datetime
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import linear_model
+import statsmodels.api as sm
+import scipy.stats as stats
+get_ipython().run_line_magic('matplotlib', 'inline')
+plt.style.use('seaborn-whitegrid')
+
